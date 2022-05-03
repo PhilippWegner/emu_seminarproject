069ea2dd6b57939405d86e2ae65185ef8ba369ca
pwegner <wegner@ial.ruhr>
2022-05-03 21:19:17 +0200

    umlaute

--- emu_seminarproject_client/src/Main.java
+++ emu_seminarproject_client/src/Main.java
@@ -5,27 +5,27 @@ import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-	
-	@Override
-	public void start(Stage primaryStage){
-		try {
-				primaryStage.setTitle("EMU-Anwendung");
-				FXMLLoader loader = new FXMLLoader();
-				System.out.println("LOCATION: " + getClass().getResource("gui/BasisView.fxml"));
-				loader.setLocation(getClass().getResource("gui/BasisView.fxml"));
 
-				BorderPane root = loader.load();
-				Scene scene = new Scene(root, 750, 490);
-				primaryStage.setScene(scene);
-				primaryStage.show();
-				// new BasisControl(primaryStage);
-		} catch(Exception e) {
-				e.printStackTrace();
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			primaryStage.setTitle("EMU-Anwendung");
+			FXMLLoader loader = new FXMLLoader();
+			System.out.println("LOCATION: " + getClass().getResource("gui/BasisView.fxml"));
+			loader.setLocation(getClass().getResource("gui/BasisView.fxml"));
+
+			BorderPane root = loader.load();
+			Scene scene = new Scene(root, 750, 490);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+			// new BasisControl(primaryStage);
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 	}
 
-	public static void main(String... args){
-			launch(args);
+	public static void main(String... args) {
+		launch(args);
 	}
 
 }

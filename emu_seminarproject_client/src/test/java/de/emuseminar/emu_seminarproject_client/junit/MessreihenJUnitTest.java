c0aec0f3bf8db22e46453358b21186ab68997ae5
pwegner <wegner@ial.ruhr>
2022-06-01 11:25:17 +0200

    aequivalenzklassen beschreibung

--- emu_seminarproject_client/src/test/java/de/emuseminar/emu_seminarproject_client/junit/MessreihenJUnitTest.java
+++ emu_seminarproject_client/src/test/java/de/emuseminar/emu_seminarproject_client/junit/MessreihenJUnitTest.java
@@ -30,6 +30,18 @@ class MessreihenJUnitTest {
 		this.messreihe = null;
 	}
 
+	/*
+	 * 
+	 * Äquivalenztest
+	 * Fall 01:
+	 * ID = 1
+	 * Zeitinterval: 20
+	 * Verbraucher: LED
+	 * Messgroeße: Leistung
+	 * (Indirekter Test der getter und setter Methoden von Klasse Messreihe)
+	 * Nur 1 Testcase!
+	 * 
+	 */
 	@Test
 	void constructorTest01() {
 		this.messreihe = new Messreihe(1, 20, "LED", "Leistung");
@@ -39,6 +51,22 @@ class MessreihenJUnitTest {
 				() -> Assert.assertEquals("Leistung", this.messreihe.getMessgroesse()));
 	}
 
+	/*
+	 * 
+	 * Äquivalenztest
+	 * Fall 01:
+	 * ID = 1
+	 * Zeitintervall = 20
+	 * Verbraucher = LED
+	 * Messgroesse = Leistung
+	 * 
+	 * Fall 02:
+	 * ID = 1
+	 * Zeitinterval = 20
+	 * Verbraucher = LED
+	 * Messgroesse = Arbeit
+	 * 
+	 */
 	@ParameterizedTest
 	@ValueSource(strings = { "Leistung", "Arbeit" })
 	void constructorTest02(String messgroesse) {
@@ -48,7 +76,24 @@ class MessreihenJUnitTest {
 				() -> Assert.assertEquals("LED", this.messreihe.getVerbraucher()),
 				() -> Assert.assertEquals(messgroesse, this.messreihe.getMessgroesse()));
 	}
-
+	
+	/*
+	 * 
+	 * Äquivalenzklasse
+	 * Fall 01:
+	 * ID <= 0: 
+	 * 
+	 * Fall 02:
+	 * Zeitintervall < 15: 
+	 * 
+	 * Fall 03:
+	 * Verbraucher == null:
+	 * 
+	 * Fall 04:
+	 * Alles einstellbare ist fehlerhaft:
+	 * Erster Fehler wird zurueckgemeldet (ID)
+	 * 
+	 */
 	@ParameterizedTest
 	@MethodSource("aequivalenzklassenTestParameter")
 	void aequivalenzklassenTest(int messreihenId, int zeitintervall, String verbraucher, String messgroesse,
@@ -62,7 +107,7 @@ class MessreihenJUnitTest {
 	private static Stream<Arguments> aequivalenzklassenTestParameter() {
 		return Arrays.stream(new Arguments[] {
 				Arguments.of(0, 15, "LED", "Leistung", "Die MessreihenID darf nicht kleiner als 1 sein!"),
-				Arguments.of(1, 14, "LED", "Leistung", "Das Zeitintervall darf nicht kleiner als 1 sein!"),
+				Arguments.of(1, 14, "LED", "Leistung", "Das Zeitintervall darf nicht kleiner als 15 sein!"),
 				Arguments.of(1, 15, "", "Leistung", "Der Verbraucher darf nicht leer sein!"),
 				Arguments.of(-10, 10, "", "Leistung", "Die MessreihenID darf nicht kleiner als 1 sein!"), });
 	}
